839a99680b6f9cfe3980adec4719ccb13d2f5650
Mohammed Al-Zouibi <55029011+malz97@users.noreply.github.com>
2020-02-20 12:16:29 -0500

    Updated main to run rough draft of app

--- src/songlib/app/SongLib.java
+++ src/songlib/app/SongLib.java
@@ -1,25 +1,30 @@
 package songlib.app;
 
-
 import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import songlib.view.Controller;
 
 public class SongLib extends Application{
 	@Override
 	public void start(Stage primaryStage)
 	throws IOException {
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("songlib/view/layout.fxml"));
-		GridPane root = (GridPane)loader.load();
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
+		loader.setLocation(
+				getClass().getResource("/songlib/view/test.fxml"));
+		AnchorPane root = (AnchorPane)loader.load();
+		
+		Controller controller = loader.getController();
+		controller.start(primaryStage);
+		
+		Scene scene = new Scene(root, 600, 450);
 		primaryStage.setTitle("Song Library");
-		primaryStage.setResizable(false);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
