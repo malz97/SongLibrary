e3469062035f21c50372739c86fa477cf1f3d2d7
Mohammed Al-Zouibi <55029011+malz97@users.noreply.github.com>
2020-02-20 12:21:15 -0500

    Initial controller, works for testing

--- src/songlib/view/ListController.java
+++ src/songlib/view/ListController.java
@@ -1,71 +1,83 @@
 package songlib.view;
 
-import songlib.app.*;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.text.Text;
+import java.util.Optional;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import songlib.app.Song;
+import songlib.utility.SongUtility;
 
-
-public class ListController {
+public class Controller {
 	
-	@FXML Button addBtn, deleteBtn, editBtn;
-	@FXML Text songNameInfo, artistInfo, albumInfo, yearInfo;
-	@FXML TextField songNameField, artistField, albumField, yearField;
-	@FXML ListView<Song> listView;
+	@FXML 
+	private ListView<Song> listView;
+	@FXML
+	private Button add, edit, delete;
+	@FXML
+	private TextField nameField, artistField, albumField, yearField;
+	
 	
 	private ObservableList<Song> obsList;
 	
 	public void start(Stage mainStage) {
 		//Creating an ObservableList from ArrayList
-		obsList = FXCollections.observableArrayList();
-		//** Need function to load songs from .txt file **//
+		//Loading songs from a csv file to obsList
+		obsList = FXCollections.observableArrayList(
+				SongUtility.loadToList("songs-funky.csv"));
 		listView.setItems(obsList);
 		//Initially select the first item in the list
 		listView.getSelectionModel().select(0);
-		//Setting the listener for the items
+		//Display song information
+		showSong(mainStage);
+		//Adding a listener for the list view items
 		listView
 			.getSelectionModel()
 			.selectedIndexProperty()
 			.addListener(
 					(obs, oldVal, newVal) ->
-					showSongDetail());
-	
+						showSong(mainStage));
 	}
 	
-	private void showSongDetail() {
+	private void showSong(Stage mainStage) {
 		Song s = listView.getSelectionModel().getSelectedItem();
-		if (s == null) {
-			return;
-		}
-		//Display the selected song's information
-		songNameInfo.setText(s.getName());
-		artistInfo.setText(s.getArtist());
-		//Song name and artist are required at minimum
+		nameField.setText(s.getName());
+		artistField.setText(s.getArtist());
 		if(!s.getAlbum().equals("")) {
-			albumInfo.setText(s.getAlbum());
+			albumField.setText(s.getAlbum());
+		}else {
+			albumField.clear();
+			albumField.setPromptText("No year information");
 		}
-		if(s.getYear() == 0) {
-			yearInfo.setText(Integer.toString(s.getYear()));
+		if(s.getYear() > 0) {
+			yearField.setText(String.valueOf(s.getYear()));
+		}else {
+			yearField.clear();
+			yearField.setPromptText("No album information");
 		}
-		//Input fields for adding are blank by default
-		songNameField.setText("");
-		artistField.setText("");
-		albumField.setText("");
-		yearField.setText("");
 	}
 	
 	
-	
-	
-	
+	@FXML
+	private void addSong(ActionEvent e) {
+		String name, artist;
+		
+	}
+	@FXML
+	private void editSong(ActionEvent e) {
+		artistField.setText("Working!");
+	}
+	@FXML
+	private void deleteSong(ActionEvent e) {
+		albumField.setText("Working!");
+	}
 	
 }
