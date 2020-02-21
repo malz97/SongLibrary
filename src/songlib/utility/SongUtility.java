399e0d5bb9dd3cce920f16ab23166114f812bd8a
Mohammed Al-Zouibi <55029011+malz97@users.noreply.github.com>
2020-02-21 14:34:39 -0500

    Added csv writer utility, persistence to app added

--- src/songlib/utility/SongUtility.java
+++ src/songlib/utility/SongUtility.java
@@ -1,13 +1,20 @@
 package songlib.utility;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+
+import javafx.collections.ObservableList;
 import songlib.app.Song;
 
 public class SongUtility {
 	
-	public static ArrayList<Song> loadToList(String file) {
+	public static final String FILE_PATH = "songs.csv";
+	
+	public static ArrayList<Song> loadToList(String file) throws IOException {
 		
 		ArrayList<Song> list = new ArrayList<Song>();
 		String line = "";
@@ -30,9 +37,36 @@ public class SongUtility {
 				}
 				list.add(s);
 			}
-		} catch(Exception e) {
-			e.printStackTrace();
+		} catch(FileNotFoundException e) {
+			PrintWriter csvWrite = new PrintWriter(FILE_PATH);
+			csvWrite.flush();
+			csvWrite.close();
+			return list;
 		}
 		return list;
+	}
+	
+	public static void save(ObservableList<Song> obsList) throws FileNotFoundException {
+		PrintWriter writer = new PrintWriter(FILE_PATH);
+		for (Song s : obsList) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(s.getName());
+			sb.append(",");
+			sb.append(s.getArtist());
+			sb.append(",");
+			if(!s.getAlbum().isEmpty()) {
+				sb.append(s.getAlbum());
+				sb.append(",");
+			}
+			if(s.getYear() > 0) {
+				sb.append(String.valueOf(s.getYear()));
+				sb.append(",");
+			}
+			sb.append('\n');
+			System.out.println(sb.toString());
+			writer.write(sb.toString());
+		}
+		writer.flush();
+		writer.close();
 	}
 }
