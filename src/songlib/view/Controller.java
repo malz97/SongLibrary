4ae362a2ee3bb464f2deea45e4682b584e01881d
Mohammed Al-Zouibi <55029011+malz97@users.noreply.github.com>
2020-02-20 23:53:06 -0500

    Alert dialogs added, duplicate check added.

--- src/songlib/view/Controller.java
+++ src/songlib/view/Controller.java
@@ -6,13 +6,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.effect.ImageInput;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import songlib.app.Song;
 import songlib.utility.SongUtility;
-import static java.util.Comparator.comparing;
 
 public class Controller {
 	
@@ -29,25 +31,36 @@ public class Controller {
 	public void start(Stage mainStage) {
 		//Creating an ObservableList from ArrayList
 		//Loading songs from a csv file to obsList
-		obsList = FXCollections.observableArrayList(
-				SongUtility.loadToList("src/songlib/utility/songs.csv"));
-		sortList();
+		obsList = FXCollections.observableArrayList();
 		listView.setItems(obsList);
 		//Initially select the first item in the list
-		listView.getSelectionModel().select(0);
 		//Display song information
-		showSong();
+		if(!obsList.isEmpty()) {
+			listView.getSelectionModel().select(0);
+			showSong(mainStage);
+		}
 		//Adding a listener for the list view items
 		listView
 			.getSelectionModel()
 			.selectedIndexProperty()
 			.addListener(
 					(obs, oldVal, newVal) ->
-						showSong());
+						showSong(mainStage));
+		//List is empty at start, edit and delete not valid
+		edit.setDisable(true);
+		delete.setDisable(true);
 	}
 	
-	private void showSong() {
+	private void showSong(Stage mainStage) {
 		Song s = listView.getSelectionModel().getSelectedItem();
+		//Set text fields to blank if list is empty
+		if(s == null) {
+			nameField.clear();
+			artistField.clear();
+			albumField.clear();
+			yearField.clear();
+			return;
+		}
 		nameField.setText(s.getName());
 		artistField.setText(s.getArtist());
 		if(!s.getAlbum().equals("")) {
@@ -74,27 +87,59 @@ public class Controller {
 		Optional<ButtonType> result = alert.showAndWait();
 		if(result.orElse(null) == ButtonType.OK) {
 			// User confirmed their action.
-			String name, artist, album;
+			String name, artist, album = "";
 			int year = 0;
 			name = nameField.getText();
 			artist = artistField.getText();
-			album = albumField.getText();
+			if(!albumField.getText().isEmpty()) {
+				album = albumField.getText();
+			}
 			if(!yearField.getText().isEmpty()) {
 				try {
 					year = Integer.parseInt(yearField.getText());
 				} catch (NumberFormatException nfe)  {
-					// TODO: Alert user that yearField contains some non-empty non-number.
+					Alert formatError = new Alert(AlertType.ERROR);
+					formatError.setTitle("Error!");
+					formatError.setHeaderText("Invalid format error!");
+					formatError.setContentText("Input in year field not a number!");
+					formatError.showAndWait();
+					return;
 				}
 				if(year <= 0) {
-					// TODO: Alert user that yearField contains a non-positive number.
+					Alert negativeYear = new Alert(AlertType.ERROR);
+					negativeYear.setTitle("Error!");
+					negativeYear.setHeaderText("Invalid format error!");
+					negativeYear.setContentText("Input in year field not a valid year!");
 				}
 			}
 			Song s = new Song(name, artist, album, year);
+			//Checks if song has name and artist
+			if(s.getName().isEmpty() || s.getArtist().isEmpty()) {
+				// TODO: Alert user, song doesn't have song or artist
+			}
+			//Checks for duplicates
+			for(Song x : obsList) {
+				if(x.equals(s)) {
+					Alert duplicate = new Alert(AlertType.ERROR);
+					duplicate.setTitle("Error!");
+					duplicate.setHeaderText("Duplicate error!");
+					duplicate.setContentText("Song is already contained in the library!");
+					duplicate.showAndWait();
+					return;
+				}
+			}
+			//Song is added to list, and sort
 			obsList.add(s);
 			sortList();
+			//Added song is automatically selected
+			int index = obsList.indexOf(s);
+			listView.getSelectionModel().select(index);
+			//List is populated, edit and delete is valid
+			delete.setDisable(false);
+			edit.setDisable(false);
 		} else {
-			// User canceled their action.
-			showSong();
+			//User canceled their action.
+			return;
 		}
 	}
 	@FXML
@@ -117,23 +162,44 @@ public class Controller {
 				try {
 					s.setYear(Integer.parseInt(yearField.getText()));
 				} catch (NumberFormatException nfe)  {
-					// TODO: Alert user that yearField contains some non-empty non-number.
+					Alert formatError = new Alert(AlertType.ERROR);
+					formatError.setTitle("Error!");
+					formatError.setHeaderText("Invalid format error!");
+					formatError.setContentText("Input in year field not a number!");
+					formatError.showAndWait();
+					return;
 				}
 				if(s.getYear() <= 0) {
-					// TODO: Alert user that yearField contains a non-positive number.
+					Alert negativeYear = new Alert(AlertType.ERROR);
+					negativeYear.setTitle("Error!");
+					negativeYear.setHeaderText("Invalid format error!");
+					negativeYear.setContentText("Input in year field not a valid year!");
 				}
 			} else {
 				s.setYear(0);
+			}//Checks for duplicates
+			for(Song x : obsList) {
+				if(x.equals(s)) {
+					Alert duplicate = new Alert(AlertType.ERROR);
+					duplicate.setTitle("Error!");
+					duplicate.setHeaderText("Duplicate error!");
+					duplicate.setContentText("Song is already contained in the library!");
+					duplicate.showAndWait();
+					return;
+				}
 			}
 			obsList.set(index, s);
 			sortList();
 		} else {
 			// User canceled their action.
-			showSong();
+			return;
 		}
 	}
 	@FXML
 	private void deleteSong(ActionEvent e) {
+		if(obsList.isEmpty()) {
+			return;
+		}
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Confirmation Dialog");
 		alert.setHeaderText("You clicked Delete.");
@@ -143,11 +209,12 @@ public class Controller {
 			// User confirmed their action.
 			int index = listView.getSelectionModel().getSelectedIndex();
 			obsList.remove(index);
+			listView.getSelectionModel().select(index);
 		} else {
-			// User canceled their action.
+			return;
 		}
 	}
-
+	
 	private void sortList() {
 		obsList.sort((s1, s2) -> (s1.getName() + s1.getArtist()).compareToIgnoreCase(s2.getName() + s2.getArtist()));
 	}
